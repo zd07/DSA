954828a115609e7b0ce0a7248debab3d687da0b1
zd07 <114851098+zd07@users.noreply.github.com>
2022-10-02 12:54:03 +0530

    Add files via upload

---
+++ RectanglePattern.java
@@ -0,0 +1,12 @@
+class RectanglePattern{
+	public static void main(String args[]){
+		int row=5 , col=5 ;
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=col;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+	
